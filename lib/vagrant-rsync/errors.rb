6554fee760530afbd6c2a645472fcbbcd43f227c
Bill Cromie <bill@cromie.org>
2013-12-22 18:14:41 -0500

    Forgot to define the error for when rsync fails. now we have a nice error message isntead of throwing an uncaught exception

--- lib/vagrant-rsync/errors.rb
+++ lib/vagrant-rsync/errors.rb
@@ -1,9 +1,18 @@
 module VagrantPlugins
   module Rsync
     module Errors
-      class RsyncNotAvailableError < Vagrant::Errors::VagrantError
+      class VagrantRsyncError < Vagrant::Errors::VagrantError
+         error_namespace("vagrant_rsync.errors")
+       end
+
+      class RsyncNotAvailableError < VagrantRsyncError
         error_key(:rsync_not_available)
       end
+
+      class RsyncFailed < VagrantRsyncError
+        error_key(:rsync_failed)
+      end
+
     end
   end
 end
